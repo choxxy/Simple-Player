5cb093112b1ff4a99aa0558d58155e8cf85bdb77
choxxy <jorh.choxxy@gmail.com>
2020-04-08 16:54:25 +0300

    added repository

--- app/src/main/java/com/example/simpleplayer/MainFragment.kt
+++ app/src/main/java/com/example/simpleplayer/MainFragment.kt
@@ -1,24 +1,23 @@
 package com.example.simpleplayer
 
+import AudioListAdapter
 import android.Manifest
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.loader.app.LoaderManager
-import androidx.loader.content.CursorLoader
-import androidx.loader.content.Loader
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.simpleplayer.data.repos.SongRepository
 import kotlinx.android.synthetic.main.fragment_main.*
 
 
-class MainFragment : Fragment(R.layout.fragment_main), LoaderManager.LoaderCallbacks<Cursor>,
-    OnSongClickListener {
+class MainFragment : Fragment(R.layout.fragment_main), OnSongClickListener {
 
     private val PERMISSIONS_REQUEST_READ_STORAGE: Int = 1200
 
@@ -31,6 +30,27 @@ class MainFragment : Fragment(R.layout.fragment_main), LoaderManager.LoaderCallb
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_READ_STORAGE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    loadSongs()
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+    private fun askPermission(){
         if (ContextCompat.checkSelfPermission(
                 requireActivity(),
                 Manifest.permission.READ_EXTERNAL_STORAGE
@@ -55,30 +75,15 @@ class MainFragment : Fragment(R.layout.fragment_main), LoaderManager.LoaderCallb
 
             }
         } else {
-            LoaderManager.getInstance(this).initLoader(0, null, this)
+            loadSongs()
         }
 
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>, grantResults: IntArray
-    ) {
-        when (requestCode) {
-            PERMISSIONS_REQUEST_READ_STORAGE -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    LoaderManager.getInstance(this).initLoader(0, null, this)
-                }
-                return
-            }
-            else -> {
-                // Ignore all other requests.
-            }
-        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        askPermission()
 
         mAdapter = AudioListAdapter(view.context, this)
 
@@ -90,46 +95,13 @@ class MainFragment : Fragment(R.layout.fragment_main), LoaderManager.LoaderCallb
         musicManager = MusicManager(requireContext(), player_control_view)
     }
 
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-        val baseUri: Uri = if (curFilter != null) {
-            Uri.withAppendedPath(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, Uri.encode(curFilter))
-        } else {
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        }
-
-        return CursorLoader(
-            requireContext(),
-            baseUri,
-            projection,
-            selection,
-            null,
-            "${MediaStore.Video.Media.DISPLAY_NAME} ASC" //Sort in alphabetical order based on display name.
-        )
-
+    private fun loadSongs() {
+        SongRepository.getInstance(requireContext()).getSongs()
+            .observe(viewLifecycleOwner, Observer { songs ->
+                mAdapter.setData(songs)
+            })
     }
 
-    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        data?.let {
-            mAdapter.setData(it)
-        }
-
-    }
-
-    override fun onLoaderReset(loader: Loader<Cursor>) {
-        mAdapter.setData(null)
-    }
-
-    var selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
-
-    private val projection = arrayOf(
-        MediaStore.Audio.Media._ID,
-        MediaStore.Audio.Media.ARTIST,
-        MediaStore.Audio.Media.TITLE,
-        MediaStore.Audio.Media.DISPLAY_NAME,
-        MediaStore.Audio.Media.ALBUM,
-        MediaStore.Audio.Media.ALBUM_ID
-
-    )
 
     override fun onClick(songUri: Uri) {
         musicManager.play(songUri)
---
+++ app/src/main/java/com/example/simpleplayer/data/repos/SongRepository.kt
@@ -0,0 +1,80 @@
+package com.example.simpleplayer.data.repos
+
+import android.content.ContentUris
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.simpleplayer.data.Song
+
+
+class SongRepository  constructor(private val context: Context) {
+
+    var selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
+
+    private val projection = arrayOf(
+        MediaStore.Audio.Media._ID,
+        MediaStore.Audio.Media.ARTIST,
+        MediaStore.Audio.Media.TITLE,
+        MediaStore.Audio.Media.DISPLAY_NAME,
+        MediaStore.Audio.Media.ALBUM,
+        MediaStore.Audio.Media.ALBUM_ID
+
+    )
+
+    private fun getColumnIndex(cursor: Cursor, columnName: String): Int{
+        return cursor.getColumnIndex(columnName)
+    }
+
+    fun getSongs(): LiveData<List<Song>>{
+
+        val musicLiveData = MutableLiveData<List<Song>>()
+        val musicList =  mutableListOf<Song>()
+
+        context.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            null,
+            "${MediaStore.Video.Media.DISPLAY_NAME} ASC" //Sort in alphabetical order based on display name.
+        ).use {cursor ->
+            if (cursor?.moveToFirst() == true) {
+                do {
+                    val id = cursor.getLong(getColumnIndex(cursor,MediaStore.Audio.Media._ID))
+                    val title = cursor.getString(getColumnIndex(cursor,MediaStore.Audio.Media.TITLE))
+                    val artist = cursor.getString(getColumnIndex(cursor,MediaStore.Audio.Media.ARTIST))
+                    val album = cursor.getString(getColumnIndex(cursor,MediaStore.Audio.Media.ALBUM))
+                    val albumId = cursor.getLong(getColumnIndex(cursor,MediaStore.Audio.Media.ALBUM_ID))
+                    val sArtworkUri = Uri.parse("content://media/external/audio/albumart")
+                    val albumArtUri = ContentUris.withAppendedId(sArtworkUri, albumId)
+                    musicList.add(
+                        Song(
+                            id = id,
+                            title = title,
+                            artUri = albumArtUri,
+                            album = album,
+                            artist = artist
+                        )
+                    )
+                } while (cursor.moveToNext())
+            }
+        }
+
+        return musicLiveData.apply {
+            value = musicList
+        }
+
+    }
+
+    companion object{
+        private var instance: SongRepository? = null
+
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: SongRepository(context)
+                    .also { instance = it }
+            }
+    }
+}
